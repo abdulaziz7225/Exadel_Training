e7878beeb9f93f94c8bdfbe9ea55ba0d276e7fb8
Abdulaziz Musaev <swiftv99@gmail.com>
2022-05-28 17:49:41 +0500

    Added default permission class

--- iclean/iclean/settings.py
+++ iclean/iclean/settings.py
@@ -147,14 +147,9 @@ AUTH_USER_MODEL = 'user.User'
 
 # installed settings
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.BasicAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    # ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.TokenAuthentication',
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
